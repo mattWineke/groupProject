41c7a3ec7f2d08012b760cdff3daf0b82e90f928
DesVallees <82540627+DesVallees@users.noreply.github.com>
2024-05-02 13:22:52 -0400

    Restored some functionality. Display platform images.

--- classes/Platform.py
+++ classes/Platform.py
@@ -1,163 +1,23 @@
+import pygame
 import random
-import pygame
 
 class Platform:
     # Constructor for Platform class
-    def __init__(self, x, y, width = 100, height = 15, color = "black", currentScore = 0):    
-
-        # Lists with Sprite animation frames
-        load = pygame.image.load
+    def __init__(self, x, y, currentScore = 0):           
+        # Load sprites
         platformPath = "images/platforms"
+        self.platform_sprite = pygame.image.load(f"{platformPath}/platform1.png")
 
+        # Platform's dimensions
+        self.width = self.platform_sprite.get_rect().width  # Get the width from the sprite
+        self.height = self.platform_sprite.get_rect().height  # Get the height from the sprite
+        
+        # Platform's initial coordinates
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.platform_sprite = load(f"{platformPath}/platform1.png")
-        
+
+        # Set current sprite and initialize sprite rectangle
         self.sprite_rect = self.platform_sprite.get_rect(topleft=(344, 65))
-
-        self.type = self.determinePlatformType()
-
-        self.touched = False
-        self.hasChangedScore = False
-
-        self.hasEnemy = False
-        self.hasPowerUp = False
-
-        # Place an enemy: Chances increase as score does - At 200 there is an enemy on (almost) every platform
-        if self.oneInXChances(max(5 - currentScore / 50, 1.1)):
-            self.hasEnemy = True
-
-        # Place a power-up: Chances increase as score does - Enemies still have priority over power-ups    
-        elif self.oneInXChances(max(3 - currentScore / 100, 1)):
-            self.hasPowerUp = True
-
-    # Method that gets called every frame
-    def tick(self):
-        if self.type == "moving":
-            self.movePlatform()
-
-        elif self.type == "breakable":
-            self.checkIfPlatformShouldBreak()
-
-    # Method to determine platform's type
-    def determinePlatformType(self):
-        random_number = random.randint(1, 100)
-
-        # Create a normal platform: 60% chanceimport random
-import pygame
-
-class Platform:
-    # Constructor for Platform class
-    def __init__(self, x, y, width = 100, height = 15, color = "black", currentScore = 0):    
-
-        # Lists with Sprite animation frames
-        load = pygame.image.load
-        platformPath = "images/platforms"
-
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.platform_sprite = load(f"{platformPath}/platform1.png")
-        
-        self.sprite_rect = self.platform_sprite.get_rect(topleft=(344, 65))
-
-        self.type = self.determinePlatformType()
-
-        self.touched = False
-        self.hasChangedScore = False
-
-        self.hasEnemy = False
-        self.hasPowerUp = False
-
-        # Place an enemy: Chances increase as score does - At 200 there is an enemy on (almost) every platform
-        if self.oneInXChances(max(5 - currentScore / 50, 1.1)):
-            self.hasEnemy = True
-
-        # Place a power-up: Chances increase as score does - Enemies still have priority over power-ups    
-        elif self.oneInXChances(max(3 - currentScore / 100, 1)):
-            self.hasPowerUp = True
-
-    # Method that gets called every frame
-    def tick(self):
-        if self.type == "moving":
-            self.movePlatform()
-
-        elif self.type == "breakable":
-            self.checkIfPlatformShouldBreak()
-
-    # Method to determine platform's type
-    def determinePlatformType(self):
-        random_number = random.randint(1, 100)
-
-        # Create a normal platform: 60% chance
-        if random_number < 60:
-            return "normal"
-
-        # Create a moving platform: 20% chance
-        elif random_number < 80:
-            return "moving"
-
-        # Create a breakable platform: 20% chance
-        else:
-            return "breakable"
-        
-    # There is one in {argument} chances method returns true
-    def oneInXChances(self, x):
-        return random.randint(1, 100) <= 100 / x
-
-
-    # Method that gets called every frame if it's a moving platform
-    def movePlatform(self):
-        pass # Should make the platform move either up AND down, or left and right in a loop
-
-    # Method that gets called every frame if it's a breakable platform
-    def checkIfPlatformShouldBreak(self):
-        # self.touched is set to True when player jumps on it
-        if self.touched:
-            pass # Code to break platform
-
-    # Since the collision functionality is already implemented, we don't need a function that returns the platform's information anymore.
-        if random_number < 60:
-            return "normal"
-
-        # Create a moving platform: 20% chance
-        elif random_number < 80:
-            return "moving"
-
-        # Create a breakable platform: 20% chance
-        else:
-            return "breakable"
-        
-    # There is one in {argument} chances method returns true
-    def oneInXChances(self, x):
-        return random.randint(1, 100) <= 100 / x
-
-
-    # Method that gets called every frame if it's a moving platform
-    def movePlatform(self):
-        pass # Should make the platform move either up AND down, or left and right in a loop
-
-    # Method that gets called every frame if it's a breakable platform
-    def checkIfPlatformShouldBreak(self):
-        # self.touched is set to True when player jumps on it
-        if self.touched:
-            pass # Code to break platform
-
-    # Since the collision functionality is already implemented, we don't need a function that returns the platform's information anymore.import random
-
-class Platform:
-    # Constructor for Platform class
-    def __init__(self, x, y, width = 100, height = 15, color = "white", currentScore = 0):           
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
 
         self.type = self.determinePlatformType()
 
--- main.py
+++ main.py
@@ -6,36 +6,50 @@ from classes.Player import Player
 from classes.Platform import Platform
 from classes.Enemy import Enemy
 from classes.PowerUp import PowerUp
+from classes.Database import Database
         
 # Named variables
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 800
-FRAME_RATE = 45
 
-# Won't be needed after objects get images
+# Most colors won't be needed after objects get images
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+LIGHT_GREEN = (100, 255, 100)
+WHITE = (255, 255, 255)
         
 # Initialize Pygame
 pygame.init() 
-load = pygame.image.load
-characterPath = "images/background"  
-
 surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-bg = load(f"{characterPath}/space.png")
-
 pygame.display.set_caption("Pluto's Pursuit")
 clock = pygame.time.Clock()
 
-# Player Object instance
+# Initialize Font
+pygame.font.init()
+font_size = 30
+font_margin = 20
+game_font = pygame.font.Font(None, font_size)
+
+# Load background image
+backgroundPath = "images/background" 
+bg = pygame.image.load(f"{backgroundPath}/space.png")
+
+# Database instance
+db = Database()
+
+# Player instance
 pluto = Player()
 
 # Set window icon to one of pluto's images
 pygame.display.set_icon(pluto.sprites_right[0])
 
-# Will be updated in the main loop and attached to a label on the top left
-score = 0
+# Dictionary to store dynamic variables
+DYNAMIC = {
+    "frame_rate": 45,
+    "score": 0,
+    "high_score": db.get_high_score()
+}
 
 # Lists with game objects
 PLATFORMS = []
@@ -103,20 +117,23 @@ def main():
 
         # Draw platforms
         for platform in PLATFORMS:
-            platform_rect = pygame.draw.rect(surface, platform.color, pygame.Rect(platform.x, platform.y + pluto.camera_y_offset, platform.width, platform.height))
-
-            surface.blit(platform.platform_sprite, platform_rect)
+            surface.blit(platform.platform_sprite, platform.sprite_rect)
+            platform.sprite_rect.update(platform.x, platform.y + pluto.camera_y_offset, platform.width, platform.height)
 
             # Update Platform instance every frame
             platform.tick()
 
+            # Increase score if the platform is touched for the first time
+            if platform.touched and not platform.hasChangedScore: 
+                DYNAMIC["score"] += 1
+                platform.hasChangedScore = True
+
         # Draw enemies
         for enemy in ENEMIES:
-
             surface.blit(enemy.current_sprite, enemy.sprite_rect)
-            enemy.sprite_rect.update(enemy.x, enemy.y, enemy.width, enemy.height)
+            enemy.sprite_rect.update(enemy.x, enemy.y + pluto.camera_y_offset, enemy.width, enemy.height)
 
-            # Update enemy instance every frame
+            # Update Enemy instance every frame
             enemy.tick()
 
             # Handle enemy collision with pluto
@@ -130,28 +147,49 @@ def main():
             powerup.tick()
 
             # Handle power-up collision with pluto
-            if powerup.collidedWith(pluto): powerup.applyEffect(pluto, score)
+            if powerup.collidedWith(pluto): 
+                powerup.applyEffect(pluto, DYNAMIC)
+
+                # Move power-up out of the screen so it's deleted by removeOffScreenObjects function
+                powerup.y = WINDOW_HEIGHT * 2
 
 
+        # Display current score
+        score_text = f"Score: {DYNAMIC['score']}"
+        score_surface = game_font.render(score_text, True, WHITE)
+        score_coordinates = (font_margin, font_margin)
+        surface.blit(score_surface, score_coordinates)
+
+        # Display high score
+        high_score_text = f"High Score: {max(DYNAMIC['high_score'], DYNAMIC['score'])}"
+        high_score_color = WHITE if DYNAMIC["score"] <= DYNAMIC["high_score"] else LIGHT_GREEN # Change text color if new high score is being set
+        high_score_surface = game_font.render(high_score_text, True, high_score_color)
+        high_score_text_width = high_score_surface.get_width()
+        high_score_coordinates = (WINDOW_WIDTH - high_score_text_width - font_margin, font_margin)
+        surface.blit(high_score_surface, high_score_coordinates)
         
-        
+
 
         # Update the display
         pygame.display.flip()
 
         # Set frame rate
-        clock.tick(FRAME_RATE)
+        clock.tick(DYNAMIC["frame_rate"])
+
+    # If there's a new high score, save it
+    if DYNAMIC["score"] > DYNAMIC["high_score"]: 
+        db.set_high_score(DYNAMIC["score"])
 
     # Exit game
     pygame.quit()
-    sys.exit()  
+    sys.exit()
 
 
 # Function to fill the PLATFORMS, ENEMIES and POWERUPS lists with respective instances
 def createObjects():
     PLATFORM_GAP = 200
     PADDING = 20
-    PLATFORM_WIDTH = 120
+    PLATFORM_WIDTH = 344
     CAMERA_UPPER_BOUND = -pluto.camera_y_offset
 
     last_platform_y_position = PLATFORMS[-1].y if PLATFORMS else WINDOW_HEIGHT - PADDING
@@ -160,7 +198,7 @@ def createObjects():
     if not PLATFORMS or last_platform_y_position > CAMERA_UPPER_BOUND:
         new_platform_y_position = last_platform_y_position - PLATFORM_GAP
         new_platform_x_position = random.randint(PADDING, WINDOW_WIDTH - PLATFORM_WIDTH - PADDING)
-        platform_instance = Platform(new_platform_x_position, new_platform_y_position, PLATFORM_WIDTH)
+        platform_instance = Platform(new_platform_x_position, new_platform_y_position, currentScore = DYNAMIC["score"])
 
         PLATFORMS.append(platform_instance)
 
@@ -168,7 +206,7 @@ def createObjects():
         if platform_instance.hasEnemy:
             possible_x_values = [platform_instance.x, platform_instance.x + platform_instance.width]
             y_position = platform_instance.y
-            enemy_instance = Enemy(possible_x_values, y_position) # enemies # space mite img needed
+            enemy_instance = Enemy(possible_x_values, y_position)
             
             ENEMIES.append(enemy_instance)
 
@@ -185,7 +223,7 @@ def removeOffScreenObjects():
     global PLATFORMS, ENEMIES, POWERUPS
     CAMERA_LOWER_BOUND = WINDOW_HEIGHT - pluto.camera_y_offset
 
-    # Filter out the platforms, enemies and power-ups whose y-coordinate is below the camera's lower bound
+    # Filter out the platforms, enemies, and power-ups whose y-coordinate is below the camera's lower bound
     PLATFORMS = [platform for platform in PLATFORMS if platform.y < CAMERA_LOWER_BOUND]
     ENEMIES = [enemy for enemy in ENEMIES if enemy.y < CAMERA_LOWER_BOUND]
     POWERUPS = [powerup for powerup in POWERUPS if powerup.y < CAMERA_LOWER_BOUND]
